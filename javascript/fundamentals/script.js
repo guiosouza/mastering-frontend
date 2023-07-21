76c99f6f1081757a0b04e38f9f1f48cad0163d88
unknown <guilhermedeoliveiradesouza@gmail.com>
2023-07-21 17:03:50 -0300

    strict mode, fundamentals

---
+++ javascript/fundamentals/script.js
@@ -0,0 +1,9 @@
+"use strict";
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriverLicense = true; // there some error here that "strict mode" helps to see
+if (hasDriversLicense) console.log("I can drive");
+
+// const interface = "audio"; -------- "strict mode" also show an error for reserverds words of JS
